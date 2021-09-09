d39cf4938e915f9f611457cd73594c78fae2bd41
estebanveraart <esteban.veraart@gmail.com>
2021-09-09 15:02:45 +0200

    Modification exercice 3.1

--- 3.1.js
+++ 3.1.js
@@ -1,8 +1,7 @@
 let arr = [1,2,3,4,5];
-console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]);
-arr[0]=100;
-arr[1]=101;
-arr[2]=102;
-arr[3]=0;
-arr[4]=0;
-console.log(arr[0]+arr[1]+arr[2])
+let n = arr.length
+let somme = 0
+for(let i=0; i<=n; i++){
+    somme = somme + i;
+}
+console.log(somme);
