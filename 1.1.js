2ec6e4727fd2e24039049f729d646d83d2d39c35
estebanveraart <esteban.veraart@gmail.com>
2021-09-09 10:11:26 +0200

    first exercice "introduction"

---
+++ 1.1.js
@@ -0,0 +1,6 @@
+console.log("hello world")
+let age = 23;
+let namee = "Esteban";
+let firstname = "Veraart";
+let city = "Sint-Pieters-Leeuw";
+console.log( "Your name is "+ namee + " " +firstname + " and you live in " + city );
