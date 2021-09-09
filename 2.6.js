f67fd9b9ef7f04f525fc9ff00e16551fb40e8338
estebanveraart <esteban.veraart@gmail.com>
2021-09-09 11:44:32 +0200

    exercice 2.6

---
+++ 2.6.js
@@ -0,0 +1,25 @@
+let x = 1
+while(x>0,x<=7){
+    let x = prompt("choisis un nombre entier entre 1 et 7 -> jours de la semaine");
+    if(x==1){
+        console.log("lundi")
+    }
+    else if(x==2){
+        console.log("Mardi")
+    }
+    else if(x==3){
+        console.log("Mercredi")
+    }
+    else if(x==4){
+        console.log("Jeudi")
+    }
+    else if(x==5){
+        console.log("Vendredi")
+    }
+    else if(x==6){
+        console.log("Samedi")
+    }
+    else if(x==7){
+        console.log("Dimanche")
+    }
+}
