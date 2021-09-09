26a7a548e7d9484997f1d30f8117539d0c726fe0
estebanveraart <esteban.veraart@gmail.com>
2021-09-09 11:01:45 +0200

    exercice 2.2

--- 2.1.js
+++ 2.1.js
@@ -1,8 +1,8 @@
-console.log("hello");
-let age = prompt("quel est votre âge?")
+
+let age = prompt("quel est votre âge?");
 if (age>=18){
-     console.log("You are an adult")
+     console.log("You are an adult");
 }
 else{
-    console.log("You are not yet an adult")
+    console.log("You are not yet an adult");
 }
---
+++ 2.2.js
@@ -0,0 +1,9 @@
+let min = prompt("Tapez un nombre minimum");
+let max = prompt("Tapez un nombre maximum");
+if(min<max){
+    let current= prompt("tapez un nombre entre les 2 nombres choisis");
+    console.log(current)
+}
+else{
+    console.log("Je vous notifie que votre choix de nombre minimum et maximum pourrait être mieux mon cher être que tu sois h/f/a")
+}
